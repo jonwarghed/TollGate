4274cb45da18b59305c10afb16af0b543b47606a
unknown <jon.warghed@gmail.com>
2014-09-30 18:02:35 +0200

    Primary Conversion

---
+++ TollGate/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TollGate
+{
+    public class Car : IVehicle
+    {
+        public string getType()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TollGate/Motorcycle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TollGate
+{
+    public class Motorcycle: IVehicle
+    {
+        public string getType()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
